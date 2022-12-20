c4dea01dd599fd86bedfb171ac888982b88859c4
Peebs <blank-p@outlook.com>
2022-12-20 11:31:53 -0600

    fund

---
+++ fund/app.js
@@ -0,0 +1,7 @@
+/*
+
+Add d3, then use this file to code a chart.
+Simplest way to include d3 is to use a CDN (a vast network of servers that host static files like css, javascript or images)
+CDNs will deliver a file from the server closest to the user 
+
+*/
